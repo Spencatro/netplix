c84f64d7b2e885929da45e3d6293204e3c9176e7
Spencer Hawkins <hawkins.spencer@gmail.com>
2014-11-12 17:33:44 -0500

    test_servlet now works!

--- renderer/test_servlet.py
+++ renderer/test_servlet.py
@@ -1,11 +1,21 @@
 __author__ = 'Spencer'
 
 import vlc
+import time
 
 instance = vlc.Instance()
 
-player = instance.media_player_new()
+sout = '#rtp{dst=104.236.30.164,port=1234,sdp=rtsp://104.236.30.164:5005/stream_name.sdp}'
 
-sout = '#rtp{dst=SERVER_IP,port=1234,sdp=rtsp://SERVER_IP:5005/stream_name.sdp}'
+instance.vlm_add_broadcast("Test Media", r'/var/www/FlaskApp/netplix/server/DATA/bella+afp.mp4', sout, 0, None, True, False)
+instance.vlm_play_media("Test Media")
 
-player.vlm_add_broadcast("Test Media", r'/var/www/FlaskApp/netplix/server/DATA/bella+afp.mp4', sout, 0, None, True, False)
+paused = False
+
+while(1):
+	command = raw_input("p toggles playback")
+	if "p" in command:
+		if paused:
+			instance.vlm_play_media("Test Media")
+		else:
+			instance.vlm_pause_media("Test Media")
