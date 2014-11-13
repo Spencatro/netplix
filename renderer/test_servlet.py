e233300302eaee851cf73b4328863d29d42bf7e7
Spencer Hawkins <hawkins.spencer@gmail.com>
2014-11-13 14:31:37 -0600

    Testing if servlet is broken

--- renderer/test_servlet.py
+++ renderer/test_servlet.py
@@ -1,11 +1,17 @@
 __author__ = 'Spencer'
 
 import vlc
-import time
 
 instance = vlc.Instance()
 
-sout = '#rtp{dst=104.236.30.164,port=1234,sdp=rtsp://104.236.30.164:5005/stream_name.sdp}'
+RENDERER_HTTP_PORT = 49678
+RENDERER_STREAM_PORT = 49679
+
+SERVER_STREAM_PORT = 49680
+SERVER_IP = "104.236.30.164"
+
+rtsp_uri = 'rtsp://'+str(SERVER_IP)+':'+str(RENDERER_STREAM_PORT)+'/'+str(id)+'.sdp'
+sout = '#rtp{dst='+SERVER_IP+',port='+str(SERVER_STREAM_PORT)+',sdp='+rtsp_uri+'}'
 
 instance.vlm_add_broadcast("Test Media", r'/var/www/FlaskApp/netplix/server/DATA/bella+afp.mp4', sout, 0, None, True, False)
 instance.vlm_play_media("Test Media")
