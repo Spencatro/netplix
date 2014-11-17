efac501ac825cfcb8abc184dca3fe7702ccb0d0c
Spencer Hawkins <hawkins.spencer@gmail.com>
2014-11-16 19:36:08 -0600

    Use curl, not wget

--- infrastructure/cron_target.py
+++ infrastructure/cron_target.py
@@ -6,4 +6,4 @@ __author__ = 'sxh112430'
 # run this once every half second. Start this script once every minute.
 for i in range((59 * 2) + 1):
     time.sleep(.5) # wait one half second
-    call(['wget','http://np.spencer-hawkins.com/cron_trigger/'])
+    call(['curl','http://np.spencer-hawkins.com/cron_trigger/'])
