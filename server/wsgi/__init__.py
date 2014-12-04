33b4957f480cec3cc85ba53cafec761b96a1a563
Spencer Hawkins (windows) <hawkins.spencer@gmail.com>
2014-12-04 13:17:57 -0800

    more hax

--- server/wsgi/__init__.py
+++ server/wsgi/__init__.py
@@ -282,9 +282,9 @@ class NetplixApp(Flask):
         db_dict = self.load_db_file()
         playing_list = self.get_playing_list()
         if len(playing_list) == 0:
-            db_dict['now_playing'] = None
-            with open(config.DB_JSON_FILE,'w') as fp:
-                json.dump(db_dict, fp)
+            # db_dict['now_playing'] = None
+            # with open(config.DB_JSON_FILE,'w') as fp:
+            #     json.dump(db_dict, fp)
             return "Stream stopped"
         for resource_id in self.get_playing_list():
             db_dict['now_playing'] = 'rtsp://'+str(config.SERVER_IP)+':'+str(config.RENDERER_STREAM_PORT)+'/'+str(resource_id)+'.sdp'
